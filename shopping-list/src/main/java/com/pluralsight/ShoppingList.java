d4ab6eda1d9f95a6869fbd8372870b6b20b86ba6
Jack Skjong-Nilsen <jskjongnilsen@gmail.com>
2025-04-10 11:32:03 -0500

    Added first maven project and shopping list

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Chicken breast");
+        System.out.println("Milk");
+        System.out.println("Cereal");
+        System.out.println("Bread");
+        System.out.println("Oatmeal");
+        System.out.println("Ground Beef");
+        System.out.println("Pizza Sauce");
+        System.out.println("Pizza dough");
+        System.out.println("Butter Chicken Sauce");
+        System.out.println("Naan");
+
+    }
+
+}
